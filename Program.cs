12fc18fe85d4363974473ebeb41f5f4c8a9da35e
oshalygin <oshalygin@gmail.com>
2016-08-17 11:28:20 -0700

    base console application initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureServiceBusTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
